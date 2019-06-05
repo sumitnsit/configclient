3cdc43ea75b47a01c5cbc4634252fd95d7815572
kapsum <sumit.kapoor@ny.email.gs.com>
2019-06-05 14:43:52 +0000

    Final checkin

---
+++ src/main/java/in/kapour/config/ConfigClient/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package in.kapour.config.ConfigClient;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile({"local","dev","uat"})
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests().anyRequest().permitAll()
+        .and()
+        .httpBasic().disable()
+        .csrf().disable();
+  }
+}
